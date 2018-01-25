5d12eddff34c059388dcf323c048d473d5a98b2f
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-01-25 17:27:58 +0100

    Check devops tools are present

--- osm/fortimail/compile_nsd.sh
+++ osm/fortimail/compile_nsd.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if ! [ -d "devops" ]; then
+  echo "Devops dir not present, cloning...."
+  git clone https://osm.etsi.org/gerrit/osm/devops
+fi
+
 ./devops/descriptor-packages/tools/generate_descriptor_pkg.sh -t nsd -N fortimail_nsd
