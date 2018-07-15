d7af9131fb546375a28cb7587e2cc647ddff2b95
Nicolas Thomas <thomnico@googlemail.com>
2018-07-15 17:57:10 +0200

    Updates to be able to try agent based cloudify but still fail

--- cloudify/fortigate-monitored/clean.sh
+++ cloudify/fortigate-monitored/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x 
 
-cfy executions start uninstall -d fortigate-monitored -p ignore_failure=true || cfy executions start uninstall -d fortigate-monitored --force -p ignore_failure=true
+cfy executions start uninstall -d fortigate-monitored || cfy executions start uninstall -d fortigate-monitored --force -p ignore_failure=true
 
 cfy deployments delete fortigate-monitored || cfy deployments delete fortigate-monitored force
 cfy blueprint delete fortigate-monitored
