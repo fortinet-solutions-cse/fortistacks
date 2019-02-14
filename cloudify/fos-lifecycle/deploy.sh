2e01e7795f01ab3bd5bc52f80ea34a4b9ad430dd
Nicolas Thomas <thomnico@googlemail.com>
2019-02-14 22:03:12 +0100

    Simplification and clean-up
    Signed-off-by: Nicolas Thomas <thomnico@googlemail.com>

--- cloudify/fos-lifecycle/deploy.sh
+++ cloudify/fos-lifecycle/deploy.sh
@@ -2,3 +2,6 @@
 cfy blueprint upload -b lcm blueprint.yaml
 cfy deployment create --skip-plugins-validation lcm -b lcm -i inputs-citycloud.yaml
 cfy -v executions start -d lcm install
+
+## To scale
+#cfy executions start -d lcm scale --dry-run -p  scalable_entity_name=fos_fips -p delta=2
