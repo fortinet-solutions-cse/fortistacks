fbeef4741a990f57023d5ca151c4b0fbfdc41185
thomnico <thomnico@googlemail.com>
2019-08-10 10:55:35 +0200

    Cleaning fortistacks initial ubuntu setup for Openstack in a box. Correct links to have a README.md while browsing github
    Signed-off-by: thomnico <thomnico@googlemail.com>

--- lxc-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-#Script to destroy all lxc instances in case it is needed (juju disconnected for exmaple)
-for l in `lxc list  --format json | jq .[].name --raw-output`
- do
-  lxc delete $l --force
- done
