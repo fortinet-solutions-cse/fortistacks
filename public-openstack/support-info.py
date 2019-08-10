ea6ceccf5472a1df8207369b309e90dc8661bc6e
thomnico <thomnico@googlemail.com>
2019-08-10 09:29:56 +0200

    WIP
    Signed-off-by: thomnico <thomnico@googlemail.com>

--- public-openstack/support-info.py
+++ public-openstack/support-info.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
-#ref: https://docs.openstack.org/openstacksdk/latest/user/connection.html
+# ref: https://docs.openstack.org/openstacksdk/latest/user/connection.html
 # use export OS_CACERT=/etc/ssl/certs/Fortinet_CA_SSL.pem if using SSL decription
 import openstack
 import pprint
-import json
+
 # Initialize and turn on debug logging
 openstack.enable_logging(debug=False)
 
 # Initialize cloud relying on sourcing the auth file.
 conn = openstack.connect()
 
-server=conn.compute.find_server("fgt60")
-server_infos=conn.compute.get_server(server)
+server = conn.compute.find_server("fgt60")
+server_infos = conn.compute.get_server(server)
 pprint.pprint(server_infos)
-addresses=server_infos.addresses
+addresses = server_infos.addresses
 pprint.pprint(addresses["left"])
 
 for address in addresses:
     print type(addresses[address])
     pprint.pprint(addresses[address])
     for port in addresses[address]:
-        port_mac=port['OS-EXT-IPS-MAC:mac_addr']
-        print ("port mac: "+port_mac)
+        port_mac = port['OS-EXT-IPS-MAC:mac_addr']
+        print ("port mac: " + port_mac)
         for port in conn.network.ports(mac_address=port_mac):
             pprint.pprint(conn.network.get_port(port).to_dict())
 ### must separate floating ips
