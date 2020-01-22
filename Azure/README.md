9323a76e9ba74f514b642181008437e59f6308b8
Nicolas Thomas <thomnico@googlemail.com>
2020-01-22 11:22:16 +0100

    Going through the steps (draft/notes use at own risk) adding more infos/links. experiment works Azure clis use a prefix "nthomas" as AZ shared account makes it complex
    Signed-off-by: Nicolas Thomas <thomnico@googlemail.com>

--- Azure/README.md
+++ Azure/README.md
@@ -1,5 +1,5 @@
 # References
-
+- https://docs.microsoft.com/en-gb/azure/aks/private-clusters
 - https://eventmarketing.blob.core.windows.net/mstechsummit2018-after/CI32_PDF_TS18.pdf
 - https://medium.com/@tufin/integrating-aks-cluster-with-azure-firewall-b3e56d163866
 
@@ -7,3 +7,27 @@
 
 - https://github.com/weaveworks/kured
 - https://github.com/Azure/k8s-best-practices/blob/master/Security.md
+
+## Certificats 
+
+Extract cluster CA:
+kubectl config view --minify --raw --output 'jsonpath={..cluster.certificate-authority-data}' | base64 -d | openssl x509 -text -out -
+
+# Networking
+https://github.com/microsoft/Networking-with-Kubernetes-Azure
+
+# Token
+https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/
+###How to get token for K8S connector: 
+
+TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 --decode)
+
+
+--network-plugin azure --network-policy Calico
+
+
+https://docs.microsoft.com/en-us/azure/aks/use-network-policies
+
+## fix/bug ?
+src: https://github.com/Azure/AKS/issues/400
+az network vnet subnet update -n ${azurerm_subnet.aks_subnet.name} -g ${azurerm_resource_group.aks_resource_group.name} --vnet-name ${azurerm_virtual_network.aks_virtualnetwork.name} --route-table $(az resource list --resource-group MC_${azurerm_resource_group.aks_resource_group.name}_${azurerm_kubernetes_cluster.aks_cluster.name}_<YOUR LOCATION HERE> --resource-type Microsoft.Network/routeTables --query '[].{ID:id}' -o tsv)"
--- Azure/Secure-AKS.sh
+++ Azure/Secure-AKS.sh
@@ -1,6 +1,25 @@
+#!/bin/bash -ex
+#
+#    Secured AKS deployment
+#    Copyright (C) 2016 Fortinet  Ltd.
+#
+#    Authors: Nicolas Thomss  <nthomasfortinet.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Be sure to have login (az login) first
 
-GROUP_NAME="my-aks-fortistacks"
+GROUP_NAME="nthomas-aks-fortistacks"
 #REGION="francecentral"
 REGION="westeurope"
   # see https://docs.microsoft.com/en-gb/azure/aks/private-clusters
@@ -12,43 +31,55 @@ az vm image terms accept --offer fortinet_fortigate-vm_v5 --plan fortinet_fg-vm 
 
 DEPLOY_NAME=$GROUP_NAME"-FGT"
 az group deployment create --name $DEPLOY_NAME  -g $GROUP_NAME \
- --template-uri "https://raw.githubusercontent.com/fortinetsolutions/Azure-Templates/master/FortiGate/Others/Single-VM-2-NIC-Deployment/azuredeploy.json" \
+ --template-file Single-VM-2-NIC-Deployment/azuredeploy.json \
  --parameters Az-FGT-parameters.json
 
+# Ref https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/aks/use-network-policies.md
+# Create a service principal and read in the application ID
+SP=$(az ad sp create-for-rbac --output json)
+SP_ID=$(echo $SP | jq -r .appId)
+SP_PASSWORD=$(echo $SP | jq -r .password)
 
-# Reference :https://docs.microsoft.com/en-us/azure/aks/private-clusters
+# Wait 15 seconds to make sure that service principal has propagated
+echo "Waiting for service principal to propagate..."
+sleep 15
+
+# Get the virtual network resource ID
+VNET_ID=$(az network vnet show --resource-group $GROUP_NAME --name nthomas-Vnet --query id -o tsv)
+
+# Assign the service principal Contributor permissions to the virtual network resource
+az role assignment create --assignee $SP_ID --scope $VNET_ID --role Contributor
+
+
+SNET2=`az network vnet subnet list     --resource-group  $GROUP_NAME     --vnet-name nthomas-Vnet     --query "[1].id" --output tsv`
+
 # Install the aks-preview extension
 az extension add --name aks-preview
-
-az feature register --name AKSPrivateLinkPreview --namespace Microsoft.ContainerService
-# check it is
-az feature list -o tsv --query "[?contains(name, 'Microsoft.ContainerService/AKSPrivateLinkPreview')].{Name:name,State:properties.state}"
-
-#Then
-az provider register --namespace Microsoft.ContainerService
-az provider register --namespace Microsoft.Network
-az provider register --namespace Microsoft.Kubernetes
-
-# Check namespaces
-for ns in Microsoft.ContainerService Microsoft.Network Microsoft.Kubernetes
-do
-  az provider show -n $ns -o tsv --query "{Name:namespace,State:registrationState}"
-done
-
-SNET=`az network vnet subnet list     --resource-group  $GROUP_NAME     --vnet-name FortigateProtectedVnet     --query "[1].id" --output tsv`
+#remove ssh keys to ensure proper regeneration see https://docs.microsoft.com/bs-latn-ba/azure/aks/ssh otherwize
+rm -f ~/.ssh/id*
 
 az aks create \
-    --resource-group $GROUP_NAME \
+    --resource-group "$GROUP_NAME" \
     --name "secure-AKS" \
     --load-balancer-sku standard \
     --enable-private-cluster \
     --network-plugin azure \
-    --vnet-subnet-id $SNET\
-    --docker-bridge-address 172.17.0.1/16 \
-    --dns-service-ip 10.40.3.254 \
-    --service-cidr 10.40.3.0/24 \
+    --vnet-subnet-id $SNET2\
     --generate-ssh-keys \
-    --node-count 1
-
+    --node-count 2 \
+    --service-cidr 10.0.0.0/16 \
+    --dns-service-ip 10.0.0.10 \
+    --docker-bridge-address 172.17.0.1/16 \
+    --service-principal $SP_ID \
+    --client-secret $SP_PASSWORD \
+    --network-policy calico
 # Node count if quota restrictions
+az aks get-credentials --resource-group ""$GROUP_NAME""  --name "secure-AKS"
 
+az vm create \
+  --resource-group "$GROUP_NAME" \
+  --name nthomas-jumphost \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --admin-password Fortin3t-aks \
+  --subnet $SNET2  --authentication-type password
